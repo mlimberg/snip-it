d20e32987632aa9e732fbfb1448aa5fd419663e9
mlimberg <limbergmike@gmail.com>
2017-03-29 17:16:28 -0600

    Add ability to send filenames and path to render file Add ImageCard component

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menubar = require('menubar');
+const webContents = electron.webContents;
 const mb = Menubar({
   width: 400,
   height: 500,
@@ -15,12 +16,42 @@ let mainWindow = null;
 
 mb.on('after-create-window', () => {
   mb.window.loadURL('file://' + __dirname + '/public/index.html');
+  mb.window.openDevTools();
 })
 
-mb.on('show', () => {
-  const content = 'stuff!'
-  mb.window.webContents.send('imageList', content)
+// app.on('ready', () => {
+//   console.log('ready!');
+//   app.send('imageList', 'yo!')
+// })
+
+// mb.on('show', () => {
+//   console.log('show');
+//   const pathToFile = app.getPath('desktop') + '/snip-it-images'
+//   const content = fs.readdir(pathToFile, (err, data) => {
+//     const package = {
+//       path: pathToFile,
+//       data: data
+//     }
+//     mb.window.webContents.send('imageList', package)
+//   })
+// })
+
+mb.on('ready', () => {
+  console.log('ready');
+  let package = 'hey'
+  const pathToFile = app.getPath('desktop') + '/snip-it-images'
+  fs.readdir(pathToFile, (err, data) => {
+    package = {
+      path: pathToFile,
+      data: data
+    }
+  })
+  mb.on('show', () => {
+    mb.window.webContents.send('imageList', package)
+
+  })
 })
+
 
 const enableScreenshot = () => {
   mb.window.hide();
