ee6e0d1fc63b0658476332c891c928849b7fff97
mlimberg <limbergmike@gmail.com>
2017-03-30 00:49:16 -0600

    Add drawing module

--- main.js
+++ main.js
@@ -13,32 +13,43 @@ const mb = Menubar({
 const screenshot = require('electron-screenshot-service');
 const shell = require('shelljs');
 
-let mainWindow = null;
+let editWindow = null;
 
 mb.on('after-show', () => {
-  mb.hideWindow()
   enableScreenshot();
   // mb.window.loadURL('file://' + __dirname + '/public/index.html');
   // mb.window.openDevTools();
 })
 
-const openNewImageFile = (file) => {
-  const filePath = app.getPath('desktop') + `/snip-it-images/${file}`
-  let newWindow = new BrowserWindow({ show: false });
-  fs.exists(filePath, (exists) => {
-    if(exists) {
-      newWindow.show()
-    }
-  })
-}
-
 const enableScreenshot = () => {
-  // mb.window.hide();
   const newImg = Date.now() + '.png'
   shell.exec('mkdir ~/Desktop/snip-it-images', { async: true })
   const sShot = shell.exec(`screencapture -i ~/Desktop/snip-it-images/${newImg}`, () => {
-    openNewImageFile(newImg)
+    openEditWindow(newImg)
   })
+}
+
+const openEditWindow = (file) => {
+  const filePath = app.getPath('desktop') + `/snip-it-images/${file}`
+  editWindow = new BrowserWindow({
+    show: false,
+    // backgroundColor: '#000',
+    title: 'Edit Screenshot',
+  });
+
+  fs.exists(filePath, (exists) => {
+    if(exists) {
+      editWindow.show()
+      editWindow.openDevTools();
+      editWindow.loadURL('file://' + __dirname + '/public/index.html')
+    }
+  })
+
+  editWindow.on('focus', () => {
+    console.log('show!');
+    editWindow.webContents.send('currentImg', filePath)
+  })
+
 }
 
 
